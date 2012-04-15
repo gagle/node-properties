4f980bdb4533c15e08202782ae843a5be25f62f3
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2012-04-16 00:54:21 +0200

    Added an example

---
+++ examples/example.js
@@ -0,0 +1,22 @@
+var Properties = require ("../build/properties").Properties;
+
+var properties = new Properties ();
+properties
+	.set ("p1", "v1", "Property 1")
+	.set ("p2", null, "Property 2, empty")
+	.set ("p3", "v3", null)
+	.set ("p4", null, null)
+	.store ("example.properties", "Example .properties file", function (error, stored){
+		console.log ("stored: " + stored);
+		
+		properties = new Properties ();
+		properties.load ("example.properties", function (error, loaded){
+			console.log ("loaded: " + loaded);
+			
+			var keys = properties.keys ();
+			console.log ("keys: " + keys);
+			keys.forEach (function (key){
+				console.log (key + ":" + properties.get (key));
+			});
+		});
+	});
