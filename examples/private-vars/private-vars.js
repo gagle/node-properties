1789bc62b9412e2aa2ed7fb53f12e9e7d98cd396
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2013-08-08 10:15:24 +0200

    added private-vars example

---
+++ examples/private-vars/private-vars.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var properties = require ("../../lib");
+
+var options = {
+	path: true,
+	reviver: function (key, value, section){
+		if (this.isSection) return this.assert ();
+		//If the key begins with _ the property is not added
+		if (key[0] === "_") return;
+		return this.assert ();
+	}
+};
+
+properties.parse ("private-vars", options, function (error, p){
+	if (error) return console.error (error);
+	
+	console.log (p);
+	
+	/*
+	{
+		c: 1,
+		d: 1
+	}
+	*/
+});
